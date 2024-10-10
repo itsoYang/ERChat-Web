34c4dcf5c2ceb1a522e9d4234b25f7820c7253a9
qinoy <qinoy8521@163.com>
2024-10-10 17:53:08 +0800

    feat: again 😁

--- src/components/NodeEditor.vue
+++ src/components/NodeEditor.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
   import {ref} from "vue";
+  import {Plus} from "@element-plus/icons-vue";
 
   const props = defineProps(['nodeEditorVisible', 'initNodeData']);
 
@@ -15,25 +16,68 @@
       {name: 'address', type: 'varchar(36)', comment: '学生地址'},
     ]
   })
+
+  const fieldAdd = () => {
+
+  }
+
+  const fieldDel = () => {
+
+  }
+
 </script>
 
 <template>
   <div class="node-editor">
-    <el-dialog v-model="props.nodeEditorVisible" width="800" @close="$emit('nodeEditorClose')">
+    <el-dialog v-model="props.nodeEditorVisible" width="600" @close="$emit('nodeEditorClose')">
       <div class="node-editor-content">
         <div class="node-editor-header">
-          <div class="node-editor-table-name">
-            <span>表名：</span><input v-model="props.initNodeData.tableName" />
+          <div class="node-editor-header-left">
+            <div class="node-editor-table-name">
+              <span>表名：</span>
+              <el-input
+                  v-model="props.initNodeData.tableName"
+                  style="width: 240px"
+              />
+            </div>
+            <div class="node-editor-table-comment">
+              <span>描述：</span>
+              <el-input
+                  v-model="props.initNodeData.comment"
+                  style="width: 240px"
+                  :rows="2"
+                  type="textarea"
+              />
+            </div>
           </div>
-          <div class="node-editor-table-comment">
-            <span>注释：</span><input v-model="props.initNodeData.comment" />
-          </div>
+          <div class="node-editor-header-right"></div>
         </div>
+        <el-divider/>
         <div class="node-editor-body">
+          <div class="field-operator">
+            <div class="field-add">
+              <el-button type="primary" :icon="Plus" circle size="small" @click="fieldAdd"></el-button>
+            </div>
+            <div class="field-del">
+              <el-button type="primary" :icon="Plus" circle size="small" @click="fieldDel"></el-button>
+            </div>
+          </div>
           <el-table :data="props.initNodeData.fields">
-            <el-table-column property="name" label="字段名" width="150" />
-            <el-table-column property="type" label="数据类型" width="200" />
-            <el-table-column property="comment" label="注释" />
+            <el-table-column label="字段名" width="150">
+              <template #default="scope">
+                <el-input v-model="scope.row.name" />
+              </template>
+            </el-table-column>
+            <el-table-column label="数据类型" width="200">
+              <template #default="scope">
+                <el-input v-model="scope.row.type" />
+              </template>
+            </el-table-column>
+            <el-table-column label="注释">
+              <template #default="scope">
+                <el-input v-model="scope.row.comment" />
+              </template>
+            </el-table-column>
           </el-table>
         </div>
       </div>
@@ -41,6 +85,25 @@
   </div>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+  .node-editor-header {
+    display: grid;
+    flex-direction: column;
+    grid-template-columns: 60% 40%;
+    .node-editor-table-name {
+      @include node-editor-table-item;
+    }
+    .node-editor-table-comment {
+      @include node-editor-table-item;
+    }
+  }
+  .node-editor-body {
+    .field-operator {
+      display: flex;
+      justify-content: flex-end;
+      padding: 0 10px;
+      gap: 1rem;
+    }
+  }
 </style>
---
+++ src/data/DataType.ts
@@ -0,0 +1,17 @@
+
+export const MysqlFieldType = {
+    BIT: 'BIT',
+    TINYINT: 'TINYINT',
+    SMALLINT: 'SMALLINT',
+    MEDIUMINT: 'MEDIUMINT',
+    INT: 'INT',
+    BIGINT: 'BIGINT',
+    FLOAT: 'FLOAT',
+    DOUBLE: 'DOUBLE',
+    DECIMAL: 'DECIMAL',
+    DATE: 'DATE',
+    TIME: 'TIME',
+    YEAR: 'YEAR',
+    DATETIME: 'DATETIME',
+    TIMESTAMP: 'TIMESTAMP',
+}
