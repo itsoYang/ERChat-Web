3339a934c1815b7cc11ba813aaa4280037bbe51f
qinoy <qinoy8521@163.com>
2024-11-12 16:35:11 +0800

    feat: 引入uuid&设计器保存

--- src/api/designer.ts
+++ src/api/designer.ts
@@ -1,6 +1,44 @@
 import {useGraphStore} from "../stores/graph.ts";
 import {Graph} from "@antv/x6";
 import axios from "../utils/axios.ts";
+import {v4 as uuidv4} from 'uuid';
+
+interface ERDiagram {
+    id: string
+    diagramName: string
+    elements: Element[]
+}
+
+interface Element {
+    id: string | undefined
+    shape: string | undefined
+    position: IPosition
+    size: ISize
+    data: INodeData,
+    zIndex: number | undefined
+}
+
+interface IPosition {
+    x: number
+    y: number
+}
+
+interface ISize {
+    width: number
+    height: number
+}
+
+interface INodeData {
+    tableName: string
+    tableComment: string
+    fields: IField[]
+}
+
+interface IField {
+    name: string
+    type: string
+    comment: string
+}
 
 const save = async () => {
     const graphStore = useGraphStore();
@@ -13,20 +51,31 @@ const save = async () => {
     // 处理保存的数据
     const cells = graph.toJSON().cells;
 
-    let result: any[] = [];
+    console.log('保存前',cells, 'ss', JSON.stringify(cells));
+
+    const diagramId = uuidv4()
+    const diagram: ERDiagram = {
+        id: diagramId,
+        diagramName: 'test_'+diagramId,
+        elements: []
+    }
+
+    const elements: Element[] = [];
     cells.forEach(cell => {
-        const {id, position, size, data, zIndex} = cell
-        result.push({
+        const {id,shape, position, size, data, zIndex} = cell
+        elements.push({
             id,
+            shape,
             position,
             size,
             data,
             zIndex
         })
     })
+    diagram.elements = elements
 
-    console.log('保存',JSON.stringify(result));
-    await axios.post('/designer/save', result)
+    console.log('保存后',diagram)
+    await axios.post('/designer/save', diagram)
 }
 
 export {
