b2da1b530b6a2b7d9a817ad9d7c9cf9771e38051
qinoy <qinoy8521@163.com>
2024-11-13 23:12:46 +0800

    feat: 主页样式

--- src/views/index.vue
+++ src/views/index.vue
@@ -6,6 +6,15 @@
   <div class="er-container">
     <!-- 页面头部区域 -->
     <div class="er-header">
+      <div>
+        logo
+      </div>
+      <div>
+        搜索区域
+      </div>
+      <div>
+        个人信息
+      </div>
     </div>
     <!-- 页面主体区域 -->
     <div class="er-main">
@@ -14,6 +23,7 @@
           <el-button type="primary">创建项目</el-button>
         </div>
       </div>
+      <div class="gap"></div>
       <div class="er-main-right">
         <!-- 右侧列表 -->
         <div class="er-main-right-header"></div>
@@ -26,24 +36,41 @@
 
 <style lang="scss" scoped>
   .er-container{
+    background-image: url('@/assets/images/bg01.png');
     height: 100vh;
     background-color: #f7f7fb;
+    display: flex;
+    flex-direction: column;
     .er-header {
       position: fixed;
       left: 50%;
       transform: translateX(-50%);
-      width: 100vw;
+      width: 75vw;
       height: 60px;
       background-color: #8f8fa2;
       border-radius: 5px;
       box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
       transition: all 0.3s ease;
-      padding: 20px;
+      padding: 10px;
       text-align: center;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      div{
+        flex: 1;
+      }
     }
     .er-main {
-      .er-main-left {
-
+      width: 75vw;
+      margin: 70px auto auto;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      //background-color: #24b8a6;
+      height: 85vh;
+      display: grid;
+      grid-template-columns: 1.5fr 0.2fr 3.5fr;
+      .er-main-left,.er-main-right {
+        border-radius: 5px;
+        background-color: #24b8a6;
       }
     }
   }
